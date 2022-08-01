5b8a441b18d74fd43abbd7bff37e647d7e35c245
wby <1723740538@qq.com>
2022-08-01 15:43:12 +0800

    2022-08-01 完成了注册功能

---
+++ src/test/java/com/wby/community/MailTests.java
@@ -0,0 +1,37 @@
+package com.wby.community;
+
+import com.wby.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+//启用CommunityApplication作为配置类
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("1723740538@qq.com", "TEST", "Welcome.") ;
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("1723740538@qq.com", "HTML", content);
+    }
+
+}
