7d92f4797e6c58080926b545b1cb41705b92ec6c
wby <1723740538@qq.com>
2022-08-04 10:11:52 +0800

    for test

--- src/main/java/com/wby/community/controller/AlphaController.java
+++ src/main/java/com/wby/community/controller/AlphaController.java
@@ -1,13 +1,16 @@
 package com.wby.community.controller;
 
+import com.wby.community.util.CommunityUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -121,5 +124,44 @@ public class AlphaController {
         emp2.put("salary", 1400);
         list.add(emp2);
         return list;
+    }
+
+    // cookie示例
+    @RequestMapping(path="/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        // 创建cookie
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        // 设置cookie生效范围
+        cookie.setPath("/community/alpha");
+        // 设置cookie的生存时间(秒)
+        cookie.setMaxAge(60 * 10);
+        // 发送cookie
+        response.addCookie(cookie);
+        return "setCookie";
+    }
+
+    @RequestMapping(path="/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code) {
+        System.out.println(code);
+        return "getCookie";
+    }
+
+    // session示例
+    @RequestMapping(path="/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("id", 1);
+        session.setAttribute("name", "Test");
+        return "setSession";
+    }
+
+    @RequestMapping(path="/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "getSession";
     }
 }
