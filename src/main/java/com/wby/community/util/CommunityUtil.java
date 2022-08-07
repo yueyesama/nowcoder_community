4f44afac13861881202d4db1ebfb52c433e8cf76
wby <1723740538@qq.com>
2022-08-07 20:39:07 +0800

    update

--- src/main/java/com/wby/community/util/CommunityUtil.java
+++ src/main/java/com/wby/community/util/CommunityUtil.java
@@ -1,8 +1,11 @@
 package com.wby.community.util;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.DigestUtils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class CommunityUtil {
@@ -18,6 +21,33 @@ public class CommunityUtil {
             return null;
         }
         return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+
+    public static String getJsonString(int code, String msg, Map<String, Object> map) {
+        JSONObject json = new JSONObject();
+        json.put("code", code);
+        json.put("msg", msg);
+        if (map != null) {
+            for (String key : map.keySet()) {
+                json.put(key, map.get(key));
+            }
+        }
+        return json.toJSONString();
+    }
+
+    public static String getJsonString(int code, String msg) {
+        return getJsonString(code, msg, null);
+    }
+
+    public static String getJsonString(int code) {
+        return getJsonString(code, null, null);
+    }
+
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "张三");
+        map.put("age", "23");
+        System.out.println(getJsonString(0, "ok", map));
     }
 
 }
