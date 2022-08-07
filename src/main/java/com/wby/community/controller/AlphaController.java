d8b5fe5687fdc87edb733097a9ed4ab8d80e6300
wby <1723740538@qq.com>
2022-08-07 20:16:19 +0800

    for test

--- src/main/java/com/wby/community/controller/AlphaController.java
+++ src/main/java/com/wby/community/controller/AlphaController.java
@@ -1,6 +1,7 @@
 package com.wby.community.controller;
 
 import com.wby.community.util.CommunityUtil;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -164,4 +165,14 @@ public class AlphaController {
         System.out.println(session.getAttribute("name"));
         return "getSession";
     }
+
+    // ajax示例
+    @RequestMapping(path = "/ajax", method = RequestMethod.POST)
+    @ResponseBody
+    public String testAjax(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return CommunityUtil.getJsonString(0, "ok");
+    }
+
 }
