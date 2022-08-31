736b5500860788c86a131b8d7210cb0d94282da2
wby <1723740538@qq.com>
2022-08-31 11:34:58 +0800

    add 未读消息的拦截、常量定义

--- src/main/java/com/wby/community/config/WebMvcConfig.java
+++ src/main/java/com/wby/community/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.wby.community.annotation.LoginRequired;
 import com.wby.community.controller.interceptor.AlphaInterceptor;
 import com.wby.community.controller.interceptor.LoginRequiredInterceptor;
 import com.wby.community.controller.interceptor.LoginTicketInterceptor;
+import com.wby.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -21,6 +22,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
@@ -31,6 +35,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.jpeg");
     }
 
