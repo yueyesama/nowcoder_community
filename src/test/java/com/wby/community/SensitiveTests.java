56a2c6da82c25a4a4ee36c0e36b4918f96e4d637
wby <1723740538@qq.com>
2022-08-31 09:49:09 +0800

    for test

---
+++ src/test/java/com/wby/community/SensitiveTests.java
@@ -0,0 +1,27 @@
+package com.wby.community;
+
+import com.wby.community.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+//启用CommunityApplication作为配置类
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以赌博，可以嫖娼，可以吸毒，nmsl，哈哈哈！";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "这里可以赌☆博，可以嫖◇娼，可以吸♧毒，nm※sl，哈哈哈！";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
---
+++ src/test/java/com/wby/community/TransactionTests.java
@@ -0,0 +1,29 @@
+package com.wby.community;
+
+import com.wby.community.service.AlphaService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+// 启用CommunityApplication作为配置类
+@ContextConfiguration(classes = CommunityApplication.class)
+public class TransactionTests {
+
+    @Autowired
+    private AlphaService alphaService;
+
+    @Test
+    public void testSave1() {
+        Object obj = alphaService.save1();
+        System.out.println(obj);
+    }
+
+    @Test
+    public void testSave2() {
+        Object obj = alphaService.save2();
+        System.out.println(obj);
+    }
+
+}
