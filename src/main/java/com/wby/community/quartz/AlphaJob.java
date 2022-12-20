e44ca8b409adf257f11d4258e2dd2a42c7fff336
wby <1723740538@qq.com>
2022-12-20 21:44:32 +0800

    新增quartz任务调度入门案例

---
+++ src/main/java/com/wby/community/quartz/AlphaJob.java
@@ -0,0 +1,12 @@
+package com.wby.community.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class AlphaJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println(Thread.currentThread().getName() + "：execute a quartz job.");
+    }
+}
